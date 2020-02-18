ecea021b167c6275036be93049413e26adc1732a
Chisato <c23021705@gmail.com>
2020-02-18 21:18:53 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <title>GOOD MORNING</title>
+</head>
+<body>
+    <header class="header">Good morning</header>
+</body>
+</html>
