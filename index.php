32e658f1c75a7bc75377aff77e5209dd063a57bb
Chisato <c23021705@gmail.com>
2020-02-19 21:43:38 +0900

    朝ごはん写真追加

--- index.php
+++ index.php
@@ -9,4 +9,60 @@
 <body>
     <header class="header">Good Morning</header>
 </body>
+<section class="introduce">
+    <p class="introduce-comment">
+        今日は何を食べよう
+    </p>
+</section>
+<section class="breakfast">
+    <h2 class="heading">
+        <div class="breakfast-wrapper">
+            <div class="breakfast-box">
+                <img class="breakfast-image" alt="朝ごはん" src="./images/morning-01.jpg">
+                <div class="breakfast-description">
+                    <div class="breakfast-desctiprion-innner">
+                        <p class="breakfast-text">
+                            ひとつめの朝ごはんが入ります<br/>
+                            <a class="button" href="#">READ MORE</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="breakfast-box">
+                <img class="breakfast-image" alt="朝ごはん" src="./images/morning-02.jpg">
+                <div class="breakfast-description">
+                    <div class="breakfast-desctiprion-innner">
+                        <p class="breakfast-text">
+                            ふたつめの朝ごはんが入ります<br/> 
+                            <a class="button" href="#">READ MORE</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="breakfast-box">
+                <img class="breakfast-image" alt="朝ごはん" src="./images/morning-03.jpg">
+                <div class="breakfast-description">
+                    <div class="breakfast-desctiprion-innner">
+                        <p class="breakfast-text">
+                            みっつめの朝ごはんが入ります<br/> 
+                            <a class="button" href="#">READ MORE</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="breakfast-box">
+                <img class="breakfast-image" alt="朝ごはん" src="./images/morning-04.jpg">
+                <div class="breakfast-description">
+                    <div class="breakfast-desctiprion-innner">
+                        <p class="breakfast-text">
+                            よっつめの朝ごはんが入ります<br/> 
+                            <a class="button" href="#">READ MORE</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </h2>
+</section>
+
 </html>
